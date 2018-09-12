d24ba01fb24395b190af5e8f228572b9990bc655
Todor Davchev <t.davchev@gmail.com>
2018-09-12 18:41:33 +0100

    Tutorial 1

--- models/lstm.py
+++ models/lstm.py
@@ -16,7 +16,7 @@ def reset_graph():
 
 class BasicLSTM(object):
     def __init__(self, batch_size=50,
-                 sequence_length=8,
+                 sequence_length=10,
                  num_units=128,
                  embedding_size=128,
                  learning_rate=0.003,
